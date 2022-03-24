cee024bcef1b631432d4889394864c3d45c39a00
Igor Rosliakov <sozdayka@gmail.com>
2022-03-24 17:31:30 +0200

    up 2

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    customKey: 'my-value',
-    AUTH0_SECRET: 'viloxyf_z2GW6K4CT-KQD_MoLEA2wqv5jWuq4Jd0P7ymgG5GJGMpvMneXZzhK3s',
-    AUTH0_BASE_URL: 'https://main.d36qbm057x7wa8.amplifyapp.com',
-    AUTH0_ISSUER_BASE_URL: 'https://sozdayka.us.auth0.com',
-    AUTH0_CLIENT_ID: 'ROvjey82bOGLD2UZMUmjagniGEfzOPSf',
-    AUTH0_CLIENT_SECRET: 'tIvlJu1mT8XimZa8dlO8nw59QhaMELwhNecQCIPaSwioD5sQRG3RRUrhrVkopwfK'
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
