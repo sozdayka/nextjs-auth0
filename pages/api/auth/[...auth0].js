565fd71453d7c552c8fa2a96fd8df1e1817490df
Igor Rosliakov <sozdayka@gmail.com>
2022-04-01 22:14:30 +0300

    +4

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -82,7 +82,7 @@ export default handleAuth({
       }
 
       await handleCallback(req, res, {
-        redirectUri: `http://${orgName}/api/auth/callback`,
+        redirectUri: `https://${orgName}/api/auth/callback`,
       });
     } catch (error) {
       res.status(error.status || 400).end(error.message);
