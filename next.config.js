24c99ef292d69fe0dfc26e42fc37b6b9b7e2dc87
Igor Rosliakov <sozdayka@gmail.com>
2022-04-08 21:33:40 +0300

    upd 0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    const raw = Object.keys(process.env)
+      .reduce(
+        (env, key) => {
+          env[key] = process.env[key]
+          return env
+        },
+        {
+          AUTH0_SECRET: process.env.AUTH0_SECRET,
+          AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+          AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+          AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+          AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+          AUTH0_COOKIE_DOMAIN: process.env.AUTH0_COOKIE_DOMAIN
+        }
+      )
+
+    const env = {
+      'process.env': Object.keys(raw).reduce((env, key) => {
+        env[key] = JSON.stringify(raw[key])
+        return env
+      }, {})
+    }
+
+    config.plugins.push(
+      new options.webpack.DefinePlugin(env)
+    )
+
+    return config
+  }
 }
 
 module.exports = nextConfig
